3cd547d50af7b6bf857c759548e551ebe5a950ac
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2017-04-23 00:35:02 -0300

    Add basic CPU movement, pause functionality :octopus:
    Everything's still very rough though

--- functional_pong/Program.fs
+++ functional_pong/Program.fs
@@ -1,7 +1,7 @@
 ﻿open System
 
 let sleep(time:int) = Threading.Thread.Sleep(time);
-
+let rand = System.Random()
 let div = "              -------------------------------------"
 let board_width = 34
 let board_height = 9
@@ -19,7 +19,7 @@ let std_board = Array2D.init<int> board_height (board_width+1) (
                         || row = std_y_pos+1 && col = 0
                         || row = std_y_pos+2 && col = 0) then
                             1
-                        else if (row = 3 && col = 16) then
+                        else if (row = board_height/2 && col = board_width/2) then
                             3
                         else 0
                 )
@@ -117,7 +117,7 @@ let rec loop(state:string, height:int, board:int[,], pos:int[], dir:int[], point
     try
         if Console.WindowHeight > 5 then
             Console.Clear()
-            Console.BufferHeight <- Console.WindowHeight+5
+            Console.BufferHeight <- Console.WindowHeight+15
     with
         | _ -> ()
 
@@ -135,13 +135,22 @@ let rec loop(state:string, height:int, board:int[,], pos:int[], dir:int[], point
         printc("height", "cyan")
         printc(" > ", "darkcyan")
         printc("5  ", "cyan")
-        printf "-  Move paddle up\n\n\n\n  Resize to 5 to start: "
-        if (Console.WindowHeight - 15 >= 0) then printf "%i/5" (Console.WindowHeight - 15);
-        else printc("0/5", "darkred");
-        if (Console.WindowHeight - 15 <> 5) then
+        printf "-  Move paddle up\n\n\n\n  Resize to 10 to start: "
+        if (Console.WindowHeight - 15 >= 0) then printf "%i/10" (Console.WindowHeight - 15)
+        else printc("0/5", "darkred")
+        if (Console.WindowHeight - 15 <> 10) then
             loop(state, Console.WindowHeight, std_board, pos, dir, points)
         else
             loop("game", Console.WindowHeight, std_board, pos, dir, points)
+    else if(Console.WindowHeight - 15 > 29) then
+        print_header(state)
+        printf "%s\n" div
+        print_board(board)
+        printf "%s" div
+        printf "\n                            [ "
+        printc("GAME PAUSED", "darkyellow")
+        printf " ]\n\n                     Resize below 30 to continue\n                        Current height: %i"(Console.WindowHeight - 15)
+        loop("game", Console.WindowHeight, board, pos, dir, points)
     else if(state = "game") then
         print_header(state)
         printf "%s\n" div
@@ -150,10 +159,10 @@ let rec loop(state:string, height:int, board:int[,], pos:int[], dir:int[], point
 
         let new_dir_ball_x = (if (pos.[2] + dir.[0] > board_width-1 || pos.[2] + dir.[0] < 1) then -1 * dir.[0] else dir.[0])
         let new_pos_ball_x = (
-            if ((pos.[2] + dir.[0] = 0
-            && not (pos.[3] + dir.[1] = pos.[1] || pos.[3] + dir.[1] = pos.[1]+1 || pos.[3] + dir.[1] = pos.[1]+2))
+            if (pos.[2] + dir.[0] = 0
+            && not (pos.[3] = pos.[1] || pos.[3] = pos.[1]+1 || pos.[3] = pos.[1]+2)
             || (pos.[2] + dir.[0] = board_width
-            && not (pos.[3] + dir.[1] = pos.[0] || pos.[3] + dir.[1] = pos.[0]+1 || pos.[3] + dir.[1] = pos.[0]+2))) then
+            && not (pos.[3] = pos.[0] || pos.[3] = pos.[0]+1 || pos.[3] = pos.[0]+2))) then
                 std_x_pos
             else
                 if (pos.[2] + dir.[0] > board_width-1) then board_width-1 else if (pos.[2] + dir.[0] < 1) then 1 else pos.[2] + dir.[0])
@@ -182,13 +191,29 @@ let rec loop(state:string, height:int, board:int[,], pos:int[], dir:int[], point
             else
                 points.[1])
 
-        printf "\n              "
-        printc("CPU: ", "darkgray")
-        printf "%i                 " points_comp
-        printc("YOU: ", "gray")
+        printf "\n               "
+        printc("CPU: ", (if (points_comp > points.[1]) then "darkgreen" else "darkgray"))
+        printf "%i                       " points_comp
+        printc("YOU: ", (if (points_player > points.[0]) then "darkgreen" else "gray"))
         printf "%i" points_player
+        printf "\n\n                            "
+        if (points_comp > points.[1]) then
+            printc("CPU scores!", "darkgreen")
+            sleep(1000)
 
-        let new_pos_comp = 3 // TODO: Computer position changing and such
+        if (points_player > points.[0]) then
+            printc("Player scores!", "darkgreen")
+            sleep(1000)
+
+        let new_pos_comp = (
+            if (dir.[0] < 0 && rand.Next(2) < 1) then
+                if (dir.[1] > 0 && pos.[1] < board_height-3) then
+                    pos.[1]+1
+                else if (dir.[1] < 0 && pos.[1] > 0) then
+                    pos.[1]-1
+                else pos.[1]
+            else pos.[1]
+        )
         let new_pos_player = (if (Console.WindowHeight - 15 > limit_top && pos.[0] < board_height-3) then pos.[0]+1 else if (Console.WindowHeight - 15 < limit_bottom && pos.[0] > 0) then pos.[0]-1 else pos.[0])
         loop(
             "game",                // state:string
@@ -226,5 +251,5 @@ let main argv =
     Console.BufferWidth <- Console.WindowWidth
     Console.WindowHeight <- 30
     Console.BufferHeight <- 45
-    loop("title", Console.WindowHeight, std_board, [| std_y_pos; std_y_pos; std_x_pos; std_y_pos |], [| 1; 1 |], [| 0; 0 |])
+    loop("title", Console.WindowHeight, std_board, [| std_y_pos; std_y_pos; std_x_pos; std_y_pos |], [| -1; 1 |], [| 0; 0 |])
     0
